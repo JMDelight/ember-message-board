8845fb0e9980621b96beb2a2389fdb89b559ac72
Joel <thefencingflutist@gmail.com>
2016-08-19 11:04:54 -0700

    clean up the code and write out the README file.

--- app/routes/question.js
+++ app/routes/question.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    console.log(params);
     return this.store.findRecord('question', params.question_id);
   },
   actions: {
@@ -26,6 +25,6 @@ export default Ember.Route.extend({
     },
     deleteAnswer(answer) {
       answer.destroyRecord();
-    },
+    }
   }
 });
