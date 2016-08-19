7098df52a638e62855bf19f0b940458b6033a807
Joel <thefencingflutist@gmail.com>
2016-08-19 09:50:16 -0700

    add the functionality to add new questions and answers.

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addAnswer: false,
+  actions: {
+    saveAnswer1() {
+      var params = {
+        author: this.get('author'),
+        body: this.get('body'),
+        question: this.get('question')
+      };
+      this.set('addAnswer', false);
+      this.sendAction('saveAnswer2', params);
+    },
+    showAnswerForm() {
+      this.set('addAnswer', true);
+    }
+  }
 });
